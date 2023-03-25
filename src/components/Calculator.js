2a2c72647c3f414f4c3b9ba43f88c2c4e732c5e2
memelopez <meme30lopez@gmail.com>
2023-03-25 16:48:54 -0600

    Change Calculator to class component, add handleClick events and integrate calculatetion logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,9 @@
 // /src/components/Calculator.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import calculate from './logic/calculate';
 
 function Output(props) {
   const { result } = props;
@@ -20,36 +21,120 @@ Output.defaultProps = {
 };
 
 Output.propTypes = {
-  result: PropTypes.number,
+  result: PropTypes.string,
 };
 
-function Calculator() {
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = (value) => {
+    const { total, next, operation } = state;
+    const calcObj = {
+      total,
+      next,
+      operation,
+    };
+    const newCalcObj = calculate(calcObj, value);
+    setState({
+      total: newCalcObj.total,
+      next: newCalcObj.next,
+      operation: newCalcObj.operation,
+    });
+  };
+
+  const { total, next } = state;
+  let disp = '';
+  if (next !== null && total === null) {
+    disp = next;
+  } else if (total !== null && next !== null) {
+    disp = next;
+  } else if (next === null && total !== null) {
+    disp = total;
+  }
+
   return (
     <div className="calcContainer">
-      <Output result={0} />
+      <Output result={disp} />
       <div className="keysGrid">
-        <Button className="calcBtn" value="AC" />
-        <Button className="calcBtn" value="+/-" />
-        <Button className="calcBtn" value="%" />
-        <Button className="calcBtn" value="÷" />
-        <Button className="calcBtn" value="7" />
-        <Button className="calcBtn" value="8" />
-        <Button className="calcBtn" value="9" />
-        <Button className="calcBtn" value="x" />
-        <Button className="calcBtn" value="4" />
-        <Button className="calcBtn" value="5" />
-        <Button className="calcBtn" value="6" />
-        <Button className="calcBtn" value="-" />
-        <Button className="calcBtn" value="1" />
-        <Button className="calcBtn" value="2" />
-        <Button className="calcBtn" value="3" />
-        <Button className="calcBtn" value="+" />
-        <Button className="calcBtn" value="0" />
-        <Button className="calcBtn" value="." />
-        <Button value="=" />
+        <Button
+          value="AC"
+          handleClick={handleClick}
+        />
+        <Button
+          value="+/-"
+          handleClick={handleClick}
+        />
+        <Button
+          value="%"
+          handleClick={handleClick}
+        />
+        <Button
+          value="÷"
+          handleClick={handleClick}
+        />
+        <Button
+          value="7"
+          handleClick={handleClick}
+        />
+        <Button
+          value="8"
+          handleClick={handleClick}
+        />
+        <Button
+          value="9"
+          handleClick={handleClick}
+        />
+        <Button
+          value="x"
+          handleClick={handleClick}
+        />
+        <Button
+          value="4"
+          handleClick={handleClick}
+        />
+        <Button
+          value="5"
+          handleClick={handleClick}
+        />
+        <Button
+          value="6"
+          handleClick={handleClick}
+        />
+        <Button
+          value="-"
+          handleClick={handleClick}
+        />
+        <Button
+          value="1"
+          handleClick={handleClick}
+        />
+        <Button
+          value="2"
+          handleClick={handleClick}
+        />
+        <Button
+          value="3"
+          handleClick={handleClick}
+        />
+        <Button
+          value="+"
+          handleClick={handleClick}
+        />
+        <Button
+          value="0"
+          handleClick={handleClick}
+        />
+        <Button
+          value="."
+          handleClick={handleClick}
+        />
+        <Button
+          value="="
+          handleClick={handleClick}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default Calculator;
