8e830e75db856a5d8f9c8ab9f1a52d08d66da159
memelopez <meme30lopez@gmail.com>
2023-03-25 16:47:02 -0600

    Change button to class component and add handleClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,28 +3,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button(props) {
-  const { value } = props;
-  let classes4btn = 'calcBtn';
-  if (value === '0') {
-    classes4btn = 'calcBtn span2';
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
-  if (value === '+' || value === 'x' || value === '-' || value === '=' || value === '÷') {
-    classes4btn = 'calcBtn orangeBtn';
+
+  handleClick(e) {
+    const { handleClick } = this.props;
+    handleClick(e.target.textContent);
   }
-  return (
-    <button className={classes4btn} type="button">
-      {value}
-    </button>
-  );
+
+  render() {
+    const { value } = this.props;
+    let classes4btn = 'calcBtn';
+    if (value === '0') {
+      classes4btn = 'calcBtn span2';
+    }
+    if (value === '+' || value === 'x' || value === '-' || value === '=' || value === '÷') {
+      classes4btn = 'calcBtn orangeBtn';
+    }
+    return (
+      <button className={classes4btn} type="button" onClick={this.handleClick}>
+        {value}
+      </button>
+    );
+  }
 }
 
 Button.defaultProps = {
   value: '-1',
+  handleClick: () => {},
 };
 
 Button.propTypes = {
   value: PropTypes.string,
+  handleClick: PropTypes.func,
 };
 
 export default Button;
